8d53bb9cad6a4fa1a5da8bded07071699a0f9e16
amir kargar <amir.kargarb@gmail.com>
2018-09-06 14:31:05 -0700

    only train parameters which require gradient

--- optimizers/adam.py
+++ optimizers/adam.py
@@ -3,7 +3,8 @@ import torch.optim as O
 class Adam:
     def __init__(self, config, model):
         self.lr = config.lr
-        self.optimizer = O.Adam(model.parameters(), self.lr)
+        self.parameters = filter(lambda p: p.requires_grad, model.parameters())
+        self.optimizer = O.Adam(self.parameters, self.lr)
         self._step = 0
 
     def step(self):
