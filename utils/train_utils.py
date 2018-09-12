983d088bd2b043c41ce93d11f8aab429c6ce770d
amir kargar <amir.kargarb@gmail.com>
2018-09-12 13:06:01 -0700

    create directory before saving pytorch model

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -9,12 +9,29 @@ from config import config
 def save_model(model, config, acc, iterations):
     snapshot_prefix = os.path.join(config.save_path, 'best_snapshot')
     snapshot_path = snapshot_prefix + '_acc_{}__iter_{}_model.pt'.format(acc, iterations)
-
+    makedirs(snapshot_path)
+    
     # save model, delete previous 'best_snapshot' files
     torch.save(model, snapshot_path)
     for f in glob.glob(snapshot_prefix + '*'):
         if f != snapshot_path:
             os.remove(f)
+
+def makedirs(name):
+    """helper function for python 2 and 3 to call os.makedirs()
+       avoiding an error if the directory to be created already exists"""
+
+    import os, errno
+
+    try:
+        os.makedirs(name)
+    except OSError as ex:
+        if ex.errno == errno.EEXIST and os.path.isdir(name):
+            # ignore existing directory
+            pass
+        else:
+            # a different error happened
+            raise
 
 def log_temporary(file, line):
     with open(file, "a") as myfile:
